0f50352085e9f21b55131ff1ff72c54b18a5c671
juanfalibene <juanignaciofalibene@gmail.com>
2023-08-26 09:59:04 +0200

    created projects data file

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: "Rocio Ley",
+    subtitle: "Copywriter & Set Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    category: "Wordpress",
+    tags: ["ACF", "CSS3", "CPT"],
+    cover: ".img/project01.jpg",
+    link: "https://rocioley.com",
+  },
+  {
+    title: "Atelier Ramia",
+    subtitle: "Ceramics in Corsia",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    category: "Wordpress",
+    tags: ["ACF", "CSS3", "CPT"],
+    cover: ".img/project02.jpg",
+    link: "https://atelierramia.com",
+  },
+  {
+    title: "Especialistas Del Deporte",
+    subtitle: "NFL",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    category: "Wordpress",
+    tags: ["ACF", "CSS3", "CPT"],
+    cover: ".img/project03.jpg",
+    link: "https://especialistasdeldeporte.com",
+  },
+];
