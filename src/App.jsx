4e4ae30f972411d876bf6becb67009ef9b814a3f
juanfalibene <juanignaciofalibene@gmail.com>
2023-08-26 09:36:16 +0200

    created base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Profile from "./components/Profile";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
-  return <p>working on my new react portfolio</p>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Projects />
+      <Profile />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
