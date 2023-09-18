f6837f15d61162896cd92853f48a904801de54ab
juanfalibene <juanignaciofalibene@gmail.com>
2023-09-18 16:26:30 +0200

    Elimina node_modules del repositorio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,15 +8,19 @@ export default function Projects() {
         <h2 className="section-title-invert">Projects</h2>
         <div className="projects-list">
           {projects.map((project) => (
-            <a href={project.link} key={project.cover} className="project-link">
-              <div className="project-image">
+            <a
+              href={project.link}
+              key={`${project.title}-${project.subtitle}`}
+              className="project-link"
+            >
+              <div className="project-image" key={project.title}>
                 <img
                   className="project-img"
                   src={project.cover}
                   alt={project.title}
                 />
               </div>
-              <div className="project-info">
+              <div className="project-info" key={project.subtitle}>
                 <h3 className="project-title">
                   {project.title}
                   <span className="project-subtitle">{project.subtitle}</span>
@@ -24,8 +28,8 @@ export default function Projects() {
                 <div className="project-info-sub">
                   <h4 className="project-category">{project.category}</h4>
                   <ul className="project-tags">
-                    {project.tags.map((tag) => (
-                      <li>{tag}</li>
+                    {project.tags.map((tag, index) => (
+                      <li key={index}>{tag}</li>
                     ))}
                   </ul>
                 </div>
